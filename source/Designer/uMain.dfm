d40a981cf2455c5906e7e90ce72195b3e82f6389
Fathony Luthfillah <x2nie@yahoo.com>
2014-10-30 01:22:16 +0700

    replace to layer.paint() from image.paintBuffer

--- source/Designer/uMain.dfm
+++ source/Designer/uMain.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
-  Left = 196
-  Top = 152
-  Width = 928
-  Height = 480
+  Left = 218
+  Top = 114
+  Width = 917
+  Height = 582
   Caption = 'Form1'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -10,61 +10,402 @@ object Form1: TForm1
   Font.Height = -11
   Font.Name = 'MS Sans Serif'
   Font.Style = []
+  Menu = MainMenu1
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object ImgView: TImgView32
+    Left = 0
+    Top = 0
+    Width = 778
+    Height = 536
+    Align = alClient
+    Bitmap.ResamplerClassName = 'TNearestResampler'
+    BitmapAlign = baCustom
+    RepaintMode = rmOptimizer
+    Scale = 1.000000000000000000
+    ScaleMode = smScale
+    ScrollBars.ShowHandleGrip = True
+    ScrollBars.Style = rbsDefault
+    ScrollBars.Size = 16
+    ScrollBars.Visibility = svAuto
+    SizeGrip = sgNone
+    OverSize = 0
+    TabOrder = 2
+    TabStop = True
+  end
   object paint1: TPaintBox32
-    Left = 24
-    Top = 16
+    Left = 352
+    Top = 40
     Width = 337
     Height = 337
     TabOrder = 0
+    Visible = False
     OnMouseDown = paint1MouseDown
     OnMouseMove = paint1MouseMove
     OnMouseUp = paint1MouseUp
     OnPaintBuffer = paint1PaintBuffer
   end
-  object rgMode: TRadioGroup
-    Left = 496
-    Top = 32
-    Width = 137
-    Height = 177
-    Caption = 'rgMode'
-    ItemIndex = 0
-    Items.Strings = (
-      'Polyline'
-      'Spline'
-      'Adaptive Curve')
+  object PnlControl: TPanel
+    Left = 778
+    Top = 0
+    Width = 131
+    Height = 536
+    Align = alRight
+    BevelOuter = bvNone
     TabOrder = 1
-    OnClick = paintBoxInvalidate
+    object PnlImage: TPanel
+      Left = 0
+      Top = 0
+      Width = 131
+      Height = 130
+      Align = alTop
+      TabOrder = 0
+      object LblScale: TLabel
+        Left = 8
+        Top = 24
+        Width = 30
+        Height = 13
+        Caption = 'Scale:'
+      end
+      object ScaleCombo: TComboBox
+        Left = 16
+        Top = 40
+        Width = 105
+        Height = 21
+        DropDownCount = 9
+        ItemHeight = 13
+        TabOrder = 0
+        Text = '100%'
+        OnChange = ScaleComboChange
+        Items.Strings = (
+          '    25%'
+          '    50%'
+          '    75%'
+          '  100%'
+          '  200%'
+          '  300%'
+          '  400%'
+          '  800%'
+          '1600%')
+      end
+      object PnlImageHeader: TPanel
+        Left = 1
+        Top = 1
+        Width = 129
+        Height = 16
+        Align = alTop
+        BevelOuter = bvNone
+        Caption = 'Image Properties'
+        Color = clBtnShadow
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindow
+        Font.Height = -11
+        Font.Name = 'Tahoma'
+        Font.Style = []
+        ParentFont = False
+        TabOrder = 1
+      end
+      object CbxImageInterpolate: TCheckBox
+        Left = 16
+        Top = 72
+        Width = 97
+        Height = 17
+        Caption = 'Interpolated'
+        TabOrder = 2
+      end
+      object CbxOptRedraw: TCheckBox
+        Left = 16
+        Top = 96
+        Width = 105
+        Height = 17
+        Caption = 'Optimize Repaints'
+        Checked = True
+        State = cbChecked
+        TabOrder = 3
+      end
+    end
+    object PnlBitmapLayer: TPanel
+      Left = 0
+      Top = 130
+      Width = 131
+      Height = 247
+      Align = alTop
+      TabOrder = 1
+      object LblOpacity: TLabel
+        Left = 8
+        Top = 24
+        Width = 53
+        Height = 13
+        Caption = 'Pen Width:'
+        FocusControl = GbrLayerOpacity
+      end
+      object PnlBitmapLayerHeader: TPanel
+        Left = 1
+        Top = 1
+        Width = 129
+        Height = 16
+        Align = alTop
+        BevelOuter = bvNone
+        Caption = 'Line Properties'
+        Color = clBtnShadow
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindow
+        Font.Height = -11
+        Font.Name = 'Tahoma'
+        Font.Style = []
+        ParentFont = False
+        TabOrder = 0
+      end
+      object GbrLayerOpacity: TGaugeBar
+        Left = 16
+        Top = 40
+        Width = 105
+        Height = 12
+        Backgnd = bgPattern
+        HandleSize = 16
+        LargeChange = 10
+        Max = 16
+        Min = 1
+        ShowArrows = False
+        ShowHandleGrip = True
+        Style = rbsMac
+        Position = 16
+      end
+      object chkShowNodes: TCheckBox
+        Left = 16
+        Top = 64
+        Width = 97
+        Height = 17
+        Caption = 'Show nodes'
+        Checked = True
+        State = cbChecked
+        TabOrder = 2
+        OnClick = paintBoxInvalidate
+      end
+      object rgMode: TRadioGroup
+        Left = 10
+        Top = 96
+        Width = 111
+        Height = 81
+        Caption = 'rgMode'
+        ItemIndex = 0
+        Items.Strings = (
+          'Polyline'
+          'Spline'
+          'Adaptive Curve')
+        TabOrder = 3
+        OnClick = paintBoxInvalidate
+      end
+      object btnClear: TButton
+        Left = 16
+        Top = 192
+        Width = 105
+        Height = 17
+        Caption = 'Clear'
+        TabOrder = 4
+        OnClick = btnClearClick
+      end
+      object btnExplode: TButton
+        Left = 16
+        Top = 216
+        Width = 105
+        Height = 17
+        Caption = 'Explode'
+        TabOrder = 5
+        OnClick = btnExplodeClick
+      end
+    end
+    object PnlMagnification: TPanel
+      Left = 0
+      Top = 487
+      Width = 131
+      Height = 168
+      Align = alTop
+      TabOrder = 2
+      Visible = False
+      object LblMagifierOpacity: TLabel
+        Left = 8
+        Top = 24
+        Width = 39
+        Height = 13
+        Caption = 'Opacity:'
+      end
+      object LblMagnification: TLabel
+        Left = 8
+        Top = 64
+        Width = 66
+        Height = 13
+        Caption = 'Magnification:'
+      end
+      object LblRotation: TLabel
+        Left = 8
+        Top = 104
+        Width = 43
+        Height = 13
+        Caption = 'Rotation:'
+      end
+      object PnlMagnificationHeader: TPanel
+        Left = 1
+        Top = 1
+        Width = 129
+        Height = 16
+        Align = alTop
+        BevelOuter = bvNone
+        Caption = 'Magnifier (All) Properties'
+        Color = clBtnShadow
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindow
+        Font.Height = -11
+        Font.Name = 'Tahoma'
+        Font.Style = []
+        ParentFont = False
+        TabOrder = 0
+      end
+      object GbrMagnOpacity: TGaugeBar
+        Left = 16
+        Top = 40
+        Width = 105
+        Height = 12
+        Backgnd = bgPattern
+        HandleSize = 16
+        Max = 255
+        ShowArrows = False
+        ShowHandleGrip = True
+        Style = rbsMac
+        Position = 255
+      end
+      object GbrMagnMagnification: TGaugeBar
+        Left = 16
+        Top = 80
+        Width = 105
+        Height = 12
+        Backgnd = bgPattern
+        HandleSize = 16
+        Max = 50
+        ShowArrows = False
+        ShowHandleGrip = True
+        Style = rbsMac
+        Position = 10
+      end
+      object GbrMagnRotation: TGaugeBar
+        Left = 16
+        Top = 120
+        Width = 105
+        Height = 12
+        Backgnd = bgPattern
+        HandleSize = 16
+        Max = 180
+        Min = -180
+        ShowArrows = False
+        ShowHandleGrip = True
+        Style = rbsMac
+        Position = 0
+      end
+      object CbxMagnInterpolate: TCheckBox
+        Left = 16
+        Top = 144
+        Width = 97
+        Height = 17
+        Caption = 'Interpolated'
+        TabOrder = 4
+      end
+    end
+    object PnlButtonMockup: TPanel
+      Left = 0
+      Top = 377
+      Width = 131
+      Height = 110
+      Align = alTop
+      TabOrder = 3
+      Visible = False
+      object LblBorderRadius: TLabel
+        Left = 8
+        Top = 24
+        Width = 70
+        Height = 13
+        Caption = 'Border Radius:'
+      end
+      object LblBorderWidth: TLabel
+        Left = 8
+        Top = 64
+        Width = 65
+        Height = 13
+        Caption = 'Border Width:'
+      end
+      object PnlButtonMockupHeader: TPanel
+        Left = 1
+        Top = 1
+        Width = 129
+        Height = 16
+        Align = alTop
+        BevelOuter = bvNone
+        Caption = 'Button (All) Properties'
+        Color = clBtnShadow
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindow
+        Font.Height = -11
+        Font.Name = 'Tahoma'
+        Font.Style = []
+        ParentFont = False
+        TabOrder = 0
+      end
+      object GbrBorderRadius: TGaugeBar
+        Left = 16
+        Top = 40
+        Width = 105
+        Height = 12
+        Backgnd = bgPattern
+        HandleSize = 16
+        Max = 20
+        Min = 1
+        ShowArrows = False
+        ShowHandleGrip = True
+        Style = rbsMac
+        Position = 5
+      end
+      object GbrBorderWidth: TGaugeBar
+        Left = 16
+        Top = 80
+        Width = 105
+        Height = 12
+        Backgnd = bgPattern
+        HandleSize = 16
+        Max = 30
+        Min = 10
+        ShowArrows = False
+        ShowHandleGrip = True
+        Style = rbsMac
+        Position = 20
+      end
+    end
   end
-  object btnClear: TButton
-    Left = 696
-    Top = 32
-    Width = 75
-    Height = 25
-    Caption = 'btnClear'
-    TabOrder = 2
-    OnClick = btnClearClick
+  object MainMenu1: TMainMenu
+    Left = 104
+    Top = 40
+    object File1: TMenuItem
+      Caption = '&File'
+      object Open1: TMenuItem
+        Caption = '&Open'
+        ShortCut = 16463
+      end
+      object Save1: TMenuItem
+        Caption = '&Save'
+        ShortCut = 16467
+      end
+      object N1: TMenuItem
+        Caption = '-'
+      end
+      object SetBackground1: TMenuItem
+        Caption = 'Set &Background'
+        ShortCut = 16450
+        OnClick = SetBackground1Click
+      end
+    end
   end
-  object chkShowNodes: TCheckBox
-    Left = 496
-    Top = 224
-    Width = 97
-    Height = 17
-    Caption = 'chkShowNodes'
-    Checked = True
-    State = cbChecked
-    TabOrder = 3
-    OnClick = paintBoxInvalidate
-  end
-  object btnExplode: TButton
-    Left = 696
-    Top = 80
-    Width = 75
-    Height = 25
-    Caption = 'btnExplode'
-    TabOrder = 4
-    OnClick = btnExplodeClick
+  object dlgOpenPic1: TOpenPictureDialog
+    Left = 360
+    Top = 120
   end
 end
--- source/Designer/uMain.pas
+++ source/Designer/uMain.pas
@@ -4,15 +4,66 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, GR32_Image, GR32, GR32_Polygons, StdCtrls, ExtCtrls;
+  Dialogs,
+  GR32_Image, GR32, GR32_Layers, GR32_Polygons, GR32_Transforms,
+  StdCtrls, ExtCtrls,
+  GR32_RangeBars,
+  Menus,
+  ExtDlgs;
 
 type
+  THitTestEvent = procedure (X, Y: Integer; var Hit: Boolean) of object;
+
+  TVirtueLayer = class(TPositionedLayer)
+  private
+    FOnHitTest: THitTestEvent;
+  protected
+  protected
+    function DoHitTest(X, Y: Integer): Boolean; override;
+  public
+    property OnHitTest : THitTestEvent read FOnHitTest write FOnHitTest;
+  end;
+
   TForm1 = class(TForm)
     paint1: TPaintBox32;
+    PnlControl: TPanel;
+    PnlImage: TPanel;
+    LblScale: TLabel;
+    ScaleCombo: TComboBox;
+    PnlImageHeader: TPanel;
+    CbxImageInterpolate: TCheckBox;
+    CbxOptRedraw: TCheckBox;
+    PnlBitmapLayer: TPanel;
+    LblOpacity: TLabel;
+    PnlBitmapLayerHeader: TPanel;
+    GbrLayerOpacity: TGaugeBar;
+    chkShowNodes: TCheckBox;
+    PnlMagnification: TPanel;
+    LblMagifierOpacity: TLabel;
+    LblMagnification: TLabel;
+    LblRotation: TLabel;
+    PnlMagnificationHeader: TPanel;
+    GbrMagnOpacity: TGaugeBar;
+    GbrMagnMagnification: TGaugeBar;
+    GbrMagnRotation: TGaugeBar;
+    CbxMagnInterpolate: TCheckBox;
+    PnlButtonMockup: TPanel;
+    LblBorderRadius: TLabel;
+    LblBorderWidth: TLabel;
+    PnlButtonMockupHeader: TPanel;
+    GbrBorderRadius: TGaugeBar;
+    GbrBorderWidth: TGaugeBar;
     rgMode: TRadioGroup;
     btnClear: TButton;
-    chkShowNodes: TCheckBox;
     btnExplode: TButton;
+    ImgView: TImgView32;
+    MainMenu1: TMainMenu;
+    File1: TMenuItem;
+    Open1: TMenuItem;
+    Save1: TMenuItem;
+    N1: TMenuItem;
+    SetBackground1: TMenuItem;
+    dlgOpenPic1: TOpenPictureDialog;
     procedure paint1PaintBuffer(Sender: TObject);
     procedure paint1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
@@ -23,11 +74,24 @@ type
     procedure paintBoxInvalidate(Sender: TObject);
     procedure btnClearClick(Sender: TObject);
     procedure btnExplodeClick(Sender: TObject);
+    procedure SetBackground1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure ScaleComboChange(Sender: TObject);
   private
     { Privclate declarations }
     FNodes : TArrayOfFloatPoint;
     FCurrentIndex : Integer;
     FDragging : Boolean;
+    FLineLayer : TVirtueLayer;
+    FTransformation : TAffineTransformation;
+    procedure LayerMouseDown(Sender: TObject; Buttons: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure LayerMouseMove(Sender: TObject; Shift: TShiftState;
+      X, Y: Integer);
+    procedure LayerMouseUp(Sender: TObject; Buttons: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure PaintSimpleDrawingHandler(Sender: TObject; Buffer: TBitmap32);
+    procedure LayerHitTest(X, Y: Integer; var Hit: Boolean); 
   public
     { Public declarations }
   end;
@@ -38,7 +102,8 @@ var
 implementation
 
 uses
-  Math, GR32_Resamplers, GR32_Geometry, GR32_LowLevel, GR32_VectorUtils;
+  Math, jpeg,
+   GR32_Resamplers, GR32_Geometry, GR32_LowLevel, GR32_VectorUtils;
   
 {$R *.dfm}
 
@@ -96,14 +161,24 @@ var
 
 const
   WRAP_PROC: array[Boolean] of TWrapProc = (Clamp, Wrap);
+var  
+  flipflop : Boolean;
 begin
   WrapProc := Wrap_PROC[Closed];
   Filter := Kernel.Filter;
   R := Ceil(Kernel.GetWidth);
   H := High(Points);
 
+  flipflop := True;
   for I := 0 to H - 1 do
-    Recurse(I, GetPoint(I), GetPoint(I + 1), 0, 1);
+  begin
+    //Recurse(I, GetPoint(I), GetPoint(I + 1), 0, 1);
+    //flipflop := not flipflop;
+    if flipflop then
+      Recurse(I, GetPoint(I), GetPoint(I + 1), 0, 1)
+    else
+      AddPoint(GetPoint(I));
+  end;
 
   if Closed then
     Recurse(H, GetPoint(H), GetPoint(0), 0, 1)
@@ -129,15 +204,16 @@ var
     try
       // subdivide recursively and interpolate
       LCurve := MakeCurve(FNodes, K, False);
-      PolylineFS( paint1.Buffer, LCurve, clBlack32, False, 1.3);
+      PolylineFS( paint1.Buffer, LCurve, clBlue32, False, 1.3);
     finally
       K.Free;
     end;
 
   end;
 
-var i : Integer;
-
+var
+  i : Integer;
+  c : TColor32;
 begin
   paint1.Buffer.Clear(clWhite32);
 
@@ -148,11 +224,17 @@ begin
     //PolygonFS(paint1.Buffer, LCurve, $70000000);
     PolylineFS(paint1.Buffer,LCurve,clYellowgreen32,True,1);
     //LCurve := Ellipse(FNodes[I].X, FNodes[I].Y, 2.75, 2.75);
-    PolygonFS(paint1.Buffer, LCurve, $D000FF00);
+    if i = 0 then
+      c := clLime32
+    else if i = Length(FNodes) -1 then
+      c := clFuchsia32
+    else
+      c := clAqua32;
+    PolygonFS(paint1.Buffer, LCurve, $D0000000 or (c and $FFFFFF) );
   end;
 
   case rgMode.ItemIndex of
-    0 : PolylineFS( paint1.Buffer, FNodes, clBlack32, False, 1.3);
+    0 : PolylineFS( paint1.Buffer, FNodes, clBlue32, False, 1.3);
     2 : DrawCurve();
   end;
 //
@@ -250,6 +332,7 @@ end;
 procedure TForm1.paintBoxInvalidate(Sender: TObject);
 begin
   paint1.Invalidate;
+  FLineLayer.Changed;
 end;
 
 procedure TForm1.btnClearClick(Sender: TObject);
@@ -274,6 +357,238 @@ begin
     K.Free;
   end;
   paint1.Invalidate;
+end;
+
+procedure TForm1.SetBackground1Click(Sender: TObject);
+var P : TPicture;
+begin
+  if dlgOpenPic1.Execute then
+  begin
+    P := TPicture.Create;
+    try
+      P.LoadFromFile(dlgOpenPic1.FileName);
+      //ImgView.Bitmap.LoadFromFile(dlgOpenPic1.FileName);
+      ImgView.Bitmap.Assign(P);
+    finally
+      P.Free;
+    end;
+
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+
+  procedure CreatePositionedLayer;
+  var
+    P: TPoint;
+  begin
+    // get coordinates of the center of viewport
+    with ImgView.GetViewportRect do
+      P := ImgView.ControlToBitmap(GR32.Point((Right + Left) div 2, (Top + Bottom) div 2));
+
+    FLineLayer := TVirtueLayer.Create(ImgView.Layers);
+    FLineLayer.Location := FloatRect(0,0,1,1);
+    FLineLayer.Scaled := True;
+    FLineLayer.MouseEvents := True;
+    FLineLayer.OnMouseDown := LayerMouseDown;
+    FLineLayer.OnMouseMove := LayerMouseMove;
+    FLineLayer.OnMouseUp := LayerMouseUp;
+    FLineLayer.OnHitTest := LayerHitTest;
+    FLineLayer.OnPaint := PaintSimpleDrawingHandler;
+    //FLineLayer.OnDblClick := LayerDblClick;
+  end;
+
+begin
+  CreatePositionedLayer();
+  FTransformation := TAffineTransformation.Create;
+end;
+
+{ TVirtueLayer }
+
+function TVirtueLayer.DoHitTest(X, Y: Integer): Boolean;
+begin
+  if Assigned(FOnHitTest) then
+    FOnHitTest(X,Y, result)
+  else
+    result := inherited DoHitTest(X,Y);
+end;
+
+procedure TForm1.LayerMouseDown(Sender: TObject; Buttons: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var I,index : integer;
+  L,A : Double;
+  P : TFloatPoint;
+begin
+  P := FTransformation.ReverseTransform(FloatPoint(X,Y));
+  index := -1;
+
+  L := 1000;
+
+  for I := 0 to Length(FNodes) -1 do
+  begin
+    A := Sqr(FNodes[I].X - P.X) + Sqr(FNodes[I].Y - P.Y);
+    if (A  < 25) and (A < L) then
+    begin
+      {if (Length(FNodes) > 5) and (Button = mbRight) then
+      begin
+        if Index < Length(FNodes) - 1 then
+          Move(FNodes[Index + 1], FNodes[Index],
+            (Length(FNodes) - Index - 1) * SizeOf(TFloatPoint));
+        SetLength(FNodes, Length(FNodes) - 1);
+        PaintBox32.Invalidate;
+      end
+      else}
+      //Exit;
+      L := A;
+      index := I;
+    end;
+  end;
+
+  //DELETE
+  if (Buttons = mbRight) and (index >= 0) then
+  begin
+    for I := index to Length(FNodes) -2 do
+    begin
+      FNodes[I] := FNodes[I+1];
+    end;
+    SetLength(FNodes, Length(FNodes) - 1);
+
+    FLineLayer.Changed;
+    Exit;
+  end;
+
+  //INSERT
+  if (ssShift in Shift) and (index >= 0) then
+  begin
+    SetLength(FNodes, Length(FNodes) + 1);
+
+    for I := Length(FNodes) -1 downto index +1 do
+    begin
+      FNodes[I] := FNodes[I-1];
+    end;
+
+    Inc(index);
+  end;
+
+  FCurrentIndex := index;
+  FDragging := True;
+
+  
+  if FCurrentIndex < 0 then
+  begin
+    FCurrentIndex := Length(FNodes);
+    SetLength(FNodes, FCurrentIndex+1);
+    FNodes[FCurrentIndex] := P;//FloatPoint(X,Y);
+  end;
+
+  FLineLayer.Changed;
+end;
+
+procedure TForm1.PaintSimpleDrawingHandler(Sender: TObject;
+  Buffer: TBitmap32);
+var
+  LCurve : TArrayOfFloatPoint;
+
+
+  procedure DrawCurve();
+  var
+    K: TCustomKernel;
+  begin
+    if Length(FNodes) <=0 then
+      Exit;
+
+    // create interpolation kernel
+    K := TGaussianKernel.Create;
+    try
+      // subdivide recursively and interpolate
+      LCurve := MakeCurve(FNodes, K, False);
+      PolylineFS( Buffer, LCurve, clBlue32, False, 1.3, jsMiter, esButt, 4, FTransformation);
+    finally
+      K.Free;
+    end;
+
+  end;
+
+var
+  i : Integer;
+  c : TColor32;
+  R : TRect;
+begin
+  //Buffer.Clear(clWhite32);
+  FTransformation.Clear;
+  FTransformation.Scale(ImgView.Scale);
+  R := ImgView.GetBitmapRect;
+  FTransformation.Translate(R.Left, R.Top);
+  Buffer.FrameRectS(R, clRed32);
+
+  if chkShowNodes.Checked then
+  for I := 0 to High(FNodes) do
+  begin
+    LCurve := Circle(FNodes[I].X, FNodes[I].Y, 4);
+    //PolygonFS(paint1.Buffer, LCurve, $70000000);
+    PolylineFS(Buffer,LCurve,clYellowgreen32,True,1, jsMiter, esButt, 4, FTransformation);
+    //LCurve := Ellipse(FNodes[I].X, FNodes[I].Y, 2.75, 2.75);
+    if i = 0 then
+      c := clLime32
+    else if i = Length(FNodes) -1 then
+      c := clFuchsia32
+    else
+      c := clAqua32;
+    PolygonFS(Buffer, LCurve, $D0000000 or (c and $FFFFFF), pfAlternate, FTransformation );
+  end;
+
+  case rgMode.ItemIndex of
+    0 : PolylineFS( Buffer, FNodes, clBlue32, False, 1.3, jsMiter, esButt, 4, FTransformation);
+    2 : DrawCurve();
+  end;
+end;
+
+procedure TForm1.LayerMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+var  
+  P : TFloatPoint;
+begin
+  if FDragging then
+  begin
+    P := FTransformation.ReverseTransform(FloatPoint(X,Y));
+    FNodes[FCurrentIndex] := P;//FloatPoint(X,Y);
+
+    FLineLayer.Changed;
+
+  end;
+end;
+
+procedure TForm1.LayerMouseUp(Sender: TObject; Buttons: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if FDragging then
+  begin
+    FDragging := False;
+  end;
+end;
+
+procedure TForm1.LayerHitTest(X, Y: Integer; var Hit: Boolean);
+begin
+  Hit := True;
+end;
+
+procedure TForm1.ScaleComboChange(Sender: TObject);
+var
+  S: string;
+  I: Integer;
+begin
+  S := ScaleCombo.Text;
+  S := StringReplace(S, '%', '', [rfReplaceAll]);
+  S := StringReplace(S, ' ', '', [rfReplaceAll]);
+  if S = '' then Exit;
+  I := StrToIntDef(S, -1);
+  if (I < 1) or (I > 2000) then
+    I := Round(ImgView.Scale * 100)
+  else
+    ImgView.Scale := I * 0.01;
+  ScaleCombo.Text := IntToStr(I) + '%';
+  ScaleCombo.SelStart := Length(ScaleCombo.Text) - 1;
+
 end;
 
 end.
